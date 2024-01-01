e3b660ca66a18febc53fe6f89fada533366a8863
programin.saifulislam@gmail.com <programin.saifulislam@gmail.com>
2024-01-01 22:37:02 +0600

    create product section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import HomeBanner from "../../Components/HomeBanner/HomeBanner";
+import HomeProducts from "../../Components/HomeProducts/HomeProducts";
 
 
 const Home = () => {
     return (
-        <div>
+        <div className=" space-y-20">
             <HomeBanner></HomeBanner> 
+            <HomeProducts></HomeProducts>
         </div>
     );
 };
